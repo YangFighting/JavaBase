ca50dde40307880b7e1a6cbe41baf420c887d2cf
zhangyang03 <zhangyang03@qianxin.com>
2022-12-30 19:49:48 +0800

    【M】 Netty 入门应用

---
+++ NettyLearning/src/main/java/com/yang/netty/basic/TimeServer.java
@@ -0,0 +1,51 @@
+package com.yang.netty.basic;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+/**
+ * @author zhangyang03
+ * @Description
+ * @create 2022-12-30 19:22
+ */
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            ChannelFuture future = serverBootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    private static class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8081;
+        new TimeServer().bind(port);
+    }
+}
