66e46179c3f6bbe1a2be7af2fc6baf4c1e6760aa
zhangyang03 <zhangyang03@qianxin.com>
2023-01-02 20:14:50 +0800

    【M】 JDK 序列化和 通用二进制编码 数组大小测试

---
+++ NettyLearning/src/main/java/com/yang/netty/codec/serializable/UserInfo.java
@@ -0,0 +1,66 @@
+package com.yang.netty.codec.serializable;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+/**
+ * @author zhangyang03
+ * @Description
+ * @create 2023-01-02 20:00
+ */
+public class UserInfo implements Serializable {
+    private static final long serialVersionUID = 2L;
+    private String userName;
+    private int userID;
+
+    public UserInfo buildUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public UserInfo buildUserID(int userID) {
+        this.userID = userID;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public byte[] codeC(){
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        byte[] value = this.userName.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.userID);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+
+    public byte[] codeC(ByteBuffer buffer){
+        buffer.clear();
+        byte[] value = this.userName.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.userID);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+}
